838d942c4fc1573bf0949b643dfd1cea0f199b53
Smark16 <sengendomark16@gmail.com>
2024-08-29 14:49:52 +0300

    merge

--- src/components/geoApp.jsx
+++ src/components/geoApp.jsx
@@ -92,7 +92,7 @@ const Geo = () => {
       {lat && long && (
         <MapContainer center={[lat, long]} zoom={18
           
-        } style={{ height: "400px", width: "100%" }}>
+        } style={{ height: "100vh", width: "100%" }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
